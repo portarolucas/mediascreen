c12ae046c29bc97bbae8f84dab942fba378ef54f
Evann Gehin <evann.gehin@ezcorp.io>
2021-01-07 00:09:48 +0100

    Ajout de commentaires

--- API/public/index.php
+++ API/public/index.php
@@ -6,27 +6,34 @@ use MediaScreenAPI\Controller\Controller as API;
 
 require_once '../vendor/autoload.php';
 
+
+// Chargement de la base de données
 $config = parse_ini_file('../config/db.ini');
 $db = new \Illuminate\Database\Capsule\Manager();
 $db->addConnection($config);
 $db->setAsGlobal();
 $db->bootEloquent();
 
+
+// Slim en mode debug pour afficher le détail des erreurs
+// A désactiver en production !
 $debug = [
     'settings' => [
         'displayErrorDetails' => true,
     ],
 ];
-
 $d = new \Slim\Container($debug);
 $app = new \Slim\App($d);
 
+
+// Route : récupérations de toutes les séquences
 $app->get('/sequences', function(Request $req, Response $resp, $args) {
     $response = $resp->withHeader('Content-Type', 'application/json');
     $response->getBody()->write(API::getSequences());
     return $response;
 });
 
+// Route : récupération d'une séquence à partir de son ID
 $app->get('/sequence/{id}', function(Request $req, Response $resp, $args) {
     $id = $args['id'];
     $response = $resp->withHeader('Content-Type', 'application/json');
@@ -34,12 +41,14 @@ $app->get('/sequence/{id}', function(Request $req, Response $resp, $args) {
     return $response;
 });
 
+// Route : récupérations de tous les écrans
 $app->get('/ecrans', function(Request $req, Response $resp, $args) {
     $response = $resp->withHeader('Content-Type', 'application/json');
     $response->getBody()->write(API::getEcrans());
     return $response;
 });
 
+// Route : récupérations de tous les écrans appartenant à une même séquence à partir de l'ID de celle-ci
 $app->get('/ecrans/{id}', function(Request $req, Response $resp, $args) {
     $id = $args['id'];
     $response = $resp->withHeader('Content-Type', 'application/json');
@@ -47,6 +56,7 @@ $app->get('/ecrans/{id}', function(Request $req, Response $resp, $args) {
     return $response;
 });
 
+// Route : récupérations d'un écran à partir de son ID
 $app->get('/ecran/{id}', function(Request $req, Response $resp, $args) {
     $id = $args['id'];
     $response = $resp->withHeader('Content-Type', 'application/json');
--- API/src/MediaScreenAPI/Controller/Controller.php
+++ API/src/MediaScreenAPI/Controller/Controller.php
@@ -7,14 +7,18 @@ use MediaScreenAPI\Models\Ecran;
 
 class Controller {
 
+
+    // Méthode statique permettant de générer une erreur
     public static function setError() {
         return json_encode(['Error' => 'No data']);
     }
     
+    // Méthode statique permettant de récuperer toutes les séquences
     public static function getSequences() {
         return json_encode(Sequence::select()->get());
     }
 
+    // Méthode statique permettant de récuperer une séquence à partir de son ID
     public static function getSequence($id) {
         $sequence = Sequence::select()->where('id', '=', $id)->first();
         if(!is_null($sequence)) {
@@ -24,10 +28,12 @@ class Controller {
         }
     }
 
+    // Méthode statique permettant de récuperer tous les écrans
     public static function getEcrans() {
         return json_encode(Ecran::select()->get());
     }
 
+    // Méthode statique permettant de récuperer un écran à partir de son ID
     public static function getEcran($id) {
         $ecran = Ecran::select()->where('id', '=', $id)->first();
         if(!is_null($ecran)) {
@@ -37,6 +43,7 @@ class Controller {
         }
     }
 
+    // Méthode statique permettant de récuperer tous les écrans appartenant à une même séquence à partir de l'ID de celle-ci
     public static function getEcransSequence($id) {
         $ecrans = Ecran::select()->where('id_sequence', '=', $id)->get();
         if(!is_null($ecrans)) {
